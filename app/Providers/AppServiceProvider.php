18817d0a106bed70baa29389d6a883cc0d9b649c
Azafman <0000880867@senaimgaluno.com.br>
2025-04-10 17:01:41 -0300

    cleaning project for api only

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::unguard();
+        //desabilita a observação do atributo $fillable nos models para operações no banco de dados
+        //Exemplo de caso, ao adicionar nova coluna nuva tabela, caso não adicionar o nome da coluna no atributo $fillable no model representativo da tabela, será gerado erro
+        //Como iremos trabalhar com formRequest, teremos controle de todos os campos que serão utililzados, então esse desabilitar o $fillable não seria um problema
     }
 }

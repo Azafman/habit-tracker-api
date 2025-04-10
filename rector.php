7357d1d40ed43ffe071a9358cb79c6d2dde60421
Azafman <0000880867@senaimgaluno.com.br>
2025-04-10 17:50:30 -0300

    installing and setting up php rector

---
+++ rector.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/app',
+        __DIR__ . '/bootstrap/app.php',
+        __DIR__ . '/database',
+        __DIR__ . '/tests',
+        __DIR__ . '/routes',
+    ])//caminhos e arquivos para o rector verificar (somente os informados aqui)
+    // uncomment to reach your current PHP version
+    ->withPreparedSets(
+        deadCode: true,//remover código inutilizado
+        codeQuality: true,//qualidade de código
+        typeDeclarations: true,//força tipagem em declarações
+        privatization: true,//definir elementos privados (não entendi)
+        earlyReturn: true,//buscar retorna o mais rápido possível (evita muitos if e elses)
+        strictBooleans: true// use boleano na aplicação
+    )//
+    ->withPhpSets();//pega a configuração padrão do compose
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function (): array {
-    return [config('app.name')];
-});
+Route::get('/', fn(): array => [config('app.name')]);

a62401add4bd20f8de2b1429bdc2622dd911a18e
Azafman <0000880867@senaimgaluno.com.br>
2025-05-16 20:51:50 -0300

    crud habits in the api

--- app/Models/Habit.php
+++ app/Models/Habit.php
@@ -12,4 +12,9 @@ class Habit extends Model
     /** @use HasFactory<\Database\Factories\HabitFactory> */
     use HasFactory;//this called trait
     //assim HabitFactory será utilizado ao usar Habit::factory
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }//use column uuid to do Implicit Binding
+    //see in documentation https://laravel.com/docs/12.x/routing#customizing-the-default-key-name
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 declare(strict_types = 1);
 
+use App\Http\Controllers\HabitController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn (): array => [config('app.name')]);
+//Route::get('/', fn (): array => [config('app.name')]);
+
+Route::prefix('/api')
+->name('api.')->group(function() {
+    Route::apiResource('habits', HabitController::class);
+});
+//follow the RESTful pattern
+//see the pattern laravel: https://laravel.com/docs/12.x/controllers#resource-controllers
